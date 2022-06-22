f2246b51eaebbaf141349b1d96ce26a55746942e
MaZiYo <247948681@qq.com>
2022-06-22 17:23:22 +0800

    fix:edit text plugin content

--- src/components/plugins/LbpText.vue
+++ src/components/plugins/LbpText.vue
@@ -18,7 +18,7 @@ export default {
       {
         style,
         on: {
-          dbClick() {
+          dblclick() {
             console.log('dbClick')
             self.canEdit = true
           }
