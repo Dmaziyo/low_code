37f6f6f9954cc9117729dce72304ef9afa236487
MaZiYo <247948681@qq.com>
2022-07-06 19:27:11 +0800

    backend controller:create work entry

--- back-end/h5-api/api/work/models/work.js
+++ back-end/h5-api/api/work/models/work.js
@@ -1,8 +1,17 @@
-'use strict';
+"use strict";
 
 /**
  * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#lifecycle-hooks)
  * to customize this model
  */
 
-module.exports = {};
+module.exports = {
+  beforeCreate: async (model, attrs, options) => {
+    const defaultPages = [
+      {
+        elements: [],
+      },
+    ];
+    model.set("pages", JSON.stringify(defaultPages));
+  },
+};
